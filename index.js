d38dc53db72ca48762e4d60fb81eb6d83014937c
Randy Einfalt <einfalt@gmail.com>
2022-09-06 18:29:04 -0400

    initialize and employee creation

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const manQuestions = [
     {
         type: 'input',
         name: 'name',
-        message: 'Enter Name of Manager',
+        message: 'Enter Name of the Manager',
         validate: managerName => {
             if (managerName) {
             return true;
@@ -32,7 +32,7 @@ const manQuestions = [
 {
     type: 'number',
     name: 'id',
-    message: 'Enter the ID of new Manager',
+    message: 'Enter the ID of the new Manager',
     validate: idNumber => {
         if(idNumber) {
             return true;
@@ -45,7 +45,7 @@ const manQuestions = [
 {
     type: 'input',
     name: 'email',
-    message: 'Enter the email of new Manager',
+    message: 'Enter the email of the new Manager',
     validate: emailManager => {
         if(emailManager) {
             return true;
@@ -58,7 +58,7 @@ const manQuestions = [
 {
     type: 'number',
     name: 'officeNumber',
-    message: 'Enter the office number of new Manager',
+    message: 'Enter the office number of the new Manager',
     validate: officeNumber => {
         if(officeNumber) {
             return true;
@@ -69,4 +69,29 @@ const manQuestions = [
     }
 },
 ]
+// employee add question
+const empQuestions = [
+    {
+        type: 'list',
+        name: 'employeeAdd',
+        message: 'Do you want to add an Engineer or Intern?',
+        choices: [
+            "Yes, add Engineer",
+            "Yes, add Intern",
+            "No thanks, I'm finished"
+        ]
+    }
+]
+// function to initialize application, add manager, and adds to employeeCreate
+const managerAdd = async() => {
+    const result = await inquirer.prompt(manQuestions)
+    const managerPost = new Manager(
+        result.name,
+        result.id,
+        result.email,
+        result.officeNumber
+    );
+    employeeCreate.push(managerPost);
+    employeeAdd();
+};
 
