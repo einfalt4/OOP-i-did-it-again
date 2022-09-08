9c3a982c686ada9abdc2cdb909c96ac1f9471b9f
Randy Einfalt <einfalt@gmail.com>
2022-09-08 09:57:55 -0400

    all questions completed

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const manQuestions = [
         if(idNumber) {
             return true;
         } else {
-            console.log("Enter the ID of new Manager");
+            console.log("Enter the Employee ID of new Manager");
             return false;
         }
     }
@@ -95,3 +95,112 @@ const managerAdd = async() => {
     employeeAdd();
 };
 
+const engQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter Name of the Engineer',
+        validate: engineerName => {
+            if (engineerName) {
+            return true;
+        } else {
+            console.log("Engineer's name");
+            return false;
+        }
+    }
+},
+{
+    type: 'number',
+    name: 'id',
+    message: 'Enter the Employee ID of the new Engineer',
+    validate: idNumber => {
+        if(idNumber) {
+            return true;
+        } else {
+            console.log("Enter the ID of new Engineer");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Enter the email of the new Engineer',
+    validate: emailManager => {
+        if(emailManager) {
+            return true;
+        } else {
+            console.log("Enter the email of new Engineer");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'githubID',
+    message: 'Enter the Github username of the Engineer',
+    validate: githubID => {
+        if(githubID) {
+            return true;
+        } else {
+            console.log("Enter the engineer's Github");
+            return false;
+        }
+    }
+},
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter Name of the Intern',
+        validate: internName => {
+            if (internName) {
+            return true;
+        } else {
+            console.log("Intern's name");
+            return false;
+        }
+    }
+},
+{
+    type: 'number',
+    name: 'id',
+    message: 'Enter the Employee ID of the new Intern',
+    validate: idNumber => {
+        if(idNumber) {
+            return true;
+        } else {
+            console.log("Enter the ID of new Intern");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Enter the email of the new Intern',
+    validate: emailManager => {
+        if(emailManager) {
+            return true;
+        } else {
+            console.log("Enter the email of new Intern");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'schoolName',
+    message: 'Enter the name of the school the Intern attends',
+    validate: schoolName => {
+        if(schoolName) {
+            return true;
+        } else {
+            console.log("Enter the intern's school");
+            return false;
+        }
+    }
+},
+]
